68b3dc16879e62b6d35e5d9cc0a20dc0d2e53a62
VasylTehlivets <vasyltehlivets@gmail.com>
2022-12-09 15:27:56 +0200

    Update customers

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,9 +1,29 @@
-$('.one-time').slick({
+
+$('.responsive').slick({
   // dots: true,
-  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
-  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
+  // prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
+  // nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
   infinite: true,
   speed: 300,
-  slidesToShow: 1,
+  slidesToScroll: 1,
   adaptiveHeight: true,
+  slidesToShow: 2,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
